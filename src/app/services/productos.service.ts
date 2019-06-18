efb9d6d6f408744a041a5056a031ce381de08089
Francisca Gallardo <fran.agallardo04@gmail.com>
2019-06-17 20:23:42 -0400

    fin seccion 6

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  cargando = true;
+  productos: Producto[] = [];
+
+  constructor(private http: HttpClient) {
+    this.cargarProductos();
+   }
+
+  private cargarProductos() {
+    this.http.get('https://angular-html-4fed1.firebaseio.com/productos_idx.json')
+    .subscribe((resp: Producto[]) => {
+      console.log(resp);
+      this.productos = resp;
+      this.cargando = false;
+    });
+  }
 }
