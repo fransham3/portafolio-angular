35496bf07e9d5ab16b502eed11ebc516e90595d1
Francisca Gallardo <fran.agallardo04@gmail.com>
2019-06-17 22:08:10 -0400

    fin seccion 6

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -17,9 +17,12 @@ export class ProductosService {
   private cargarProductos() {
     this.http.get('https://angular-html-4fed1.firebaseio.com/productos_idx.json')
     .subscribe((resp: Producto[]) => {
-      console.log(resp);
       this.productos = resp;
       this.cargando = false;
     });
+  }
+
+  getProducto(id: string) {
+    return this.http.get(`https://angular-html-4fed1.firebaseio.com/productos/${id}.json`);
   }
 }
